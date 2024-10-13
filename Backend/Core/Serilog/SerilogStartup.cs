db82cf2c81063a92df5037eac5f31141050bee7a
VyacheslavDd <Sonic.x.exe@yandex.ru>
2024-10-13 17:10:11 +0500

    инициализация проекта (backend)

---
+++ Backend/Core/Serilog/SerilogStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Serilog
+{
+	public static class SerilogStartup
+	{
+		public static ConfigureHostBuilder AddSerilog(this ConfigureHostBuilder hostBuilder)
+		{
+			hostBuilder.UseSerilog((context, config) =>
+			{
+				config.ReadFrom.Configuration(context.Configuration);
+			});
+			return hostBuilder;
+		}
+	}
+}
